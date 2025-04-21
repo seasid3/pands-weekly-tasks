dc252b450b1110f16e135b9389da44833aa32d0f
Orla Woods <orla.woods@gmail.com>
2025-04-21 20:26:46 +0100

    improving code and comments

--- Week04/collatz.py
+++ Week04/collatz.py
@@ -4,27 +4,33 @@
 # The program ends if the current value is one.
 # Author: Orla Woods
 
-# Ask the user to input a positive integer
-
+# Ask the user to input a positive integer, converting the inputted string to an integer.
 number = int(input("Please enter a positive integer: ")) 
-# I got this on my own from lectures and labs # (looking back at the code)
+# I got this on my own from lectures and labs (looking back at the code)
 
-# I knew I had to: check if the number is odd or even. First I tried to do this as standalone code but that
-# was overcomplicating things and didnt work.  I wrote the general correct code (i.e., using while it's not 1, 
-# if it's even divide by 2, if it's odd multiply by 3 and add 1) but I had overcomplicated it by definining 
-# a new number.   It is confusing me that you use the same variable name throughout and dont have to redesignate
-# a new variable. But also it does make sense as you need to use it in a loop so can't keep using new variable
-# names at each itteration. 
-# In the end, I copied the code I had written into chatgpt and it showed me my errors. I had absolutely
-# overcomplicated it but was pleased to see that I had the general idea correct and i'm getting better at the 
-# syntax as the weeks go on. 
+# https://www.w3resource.com/python-exercises/challenges/1/python-challenges-1-exercise-23.php#google_vignette
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+# https://stackoverflow.com/questions/13366830/collatz-conjecture-sequence
+
+# Coming back at this at the end of the lectures, using knowledge from later lectures, I wanted to insert a 
+# loop to ensure that the program wouldn't run if the user entered an integer less than 1. 
+# https://stackoverflow.com/questions/26748083/creating-a-loop-to-check-a-variable-to-make-sure-it-is-a-positive-integer
+# https://www.reddit.com/r/learnpython/comments/yq7vkk/write_a_program_that_prompts_the_user_to_enter_an/?rdt=37874
+
+# Use a while loop to ensure the user inputs a positive integer
+# https://www.w3schools.com/python/python_while_loops.asp
+while number <= 0:
+    number = int(input("The input must be a positive integer. Please enter a positive integer: "))
+
+# Doing the Collatz Conjecture, using a while loop
+# My code wasn't working so I asked chatGPT: 
+# https://chatgpt.com/share/68069acd-eba4-800d-a0e7-f3b55e95547a
 
 while number != 1:
-    if number % 2 == 0:
+     if number % 2 == 0:
          number = number // 2
-         print(number)
-    else:
+     else:
          number = number * 3 + 1
-         print(number)
+print(number)
 
 print("Got there in the end!")
