8a9d402442a8fece536d5ff6a8e00f751ab890c3
Orla Woods <orla.woods@gmail.com>
2025-04-21 19:50:43 +0100

    making code more efficient

--- Week02/bank.py
+++ Week02/bank.py
@@ -3,13 +3,13 @@
 # a decimal point).
 # Author: Orla Woods
 
-# ask the user to input two amounts in cent.
-amount_1 = input("Enter amount 1 (in cent): ")
-amount_2 = input("Enter amount 2 (in cent): ") 
+# ask the user to input two amounts in cent, converting them to integers from strings (I was getting this error
+# before that: TypeError: unsupported operand type(s) for /: 'str' and 'int').
+amount_1 = int(input("Enter amount 1 (in cent): "))
+amount_2 = int(input("Enter amount 2 (in cent): "))
 
-# Add amount1 and amount2, converting them to integers from strings (I was getting this error before that: 
-# TypeError: unsupported operand type(s) for /: 'str' and 'int'). 
-answer = int(amount_1) + int(amount_2) 
+# Add amount1 and amount2,  
+answer = amount_1 + amount_2
 # reference: https://www.datacamp.com/tutorial/how-to-convert-a-string-into-an-integer-in-python
   
 # Divide the answer by 100 to convert it from cent to euro and round to 2 decimal places.
