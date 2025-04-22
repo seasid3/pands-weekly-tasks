95fa1ca4baa43f59c696f46242c6784536e78fd0
Orla Woods <orla.woods@gmail.com>
2025-04-22 11:48:16 +0100

    formatting README

--- Week01/helloworld.py
+++ Week01/helloworld.py
@@ -2,6 +2,8 @@
 # This program prints out Hello World!
 # Author Orla Woods
 
+# https://www.w3schools.com/python/ref_func_print.asp
+
 # print Hello World!:
 print("Hello World!")
-# reference: https://www.datacamp.com/tutorial/python-print-function
+
