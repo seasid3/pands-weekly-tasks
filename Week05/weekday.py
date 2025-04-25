a7f457eaaf6207dd6ed00b2783a1e2817c3bbdfc
Orla Woods <orla.woods@gmail.com>
2025-04-25 13:56:06 +0100

    Updating Week 5

--- Week05/weekday.py
+++ Week05/weekday.py
@@ -5,7 +5,7 @@
 # Googling how to determine what day of the week it is without user input in python:
 # https://www.shecodes.io/athena/10185-how-to-check-what-day-of-the-week-it-is-in-python
 # https://www.geeksforgeeks.org/python-program-to-find-day-of-the-week-for-a-given-date/
-# Help from ChatGPT: https://chatgpt.com/c/68069f62-4914-800d-8a49-3694d5b79891
+# Help from ChatGPT: https://chatgpt.com/share/680b862a-a638-800d-9c13-7ad88168ca38
 
 # Import datetime module
 import datetime
@@ -22,12 +22,6 @@ day_of_week = current_date.weekday()
 # Sanity check: 
 # print(day_of_week) # reverted to a comment when sanity check worked
 
-# Define a list of weekday names
-weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-
-# Sanity check: print out day of the week
-# print("Today is:", weekdays[day_of_week]) # reverted to # a comment when sanity check worked
-
 # Use an if/else statement to give an output depending on if the current day of the week is a weekday or a 
 # weekend day. 
 if day_of_week <5:
@@ -37,3 +31,11 @@ else:
 
 # Fist, I wanted to write if dayOfWeek[:5] for the weekday output but this returned an
 # error about not being able to slice integers [dayOfWeek = # currentDate.weekday() returns integers]. 
+# I was also trying to assign a day name to the day_of_week but I didnt need to do this as the integer values 
+# were all that were required.
+# Asking ChatGPT (link above) why I need datetime.datetime.today() to get the current date and time, and 
+#n not just datetime.today(), it explained that the I need to state the module name (datetime) as well as the
+# class name (datetime) in the module, which allows me to access the today() function. Without both datetimes, 
+# python wouldn't know if I was trying to reference the module or the class. I did try datetime.(today) and
+# it returned an error "AttributeError: module 'datetime' has no attribute 'today'". This makes total sense
+# to me now. 
